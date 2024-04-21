43e6ce7ff598c5e0236fcff141849c88d782bfae
DiogoLuxa <diogoluxa.dev@gmail.com>
2024-04-21 17:06:22 -0300

    ajustes usabilidade

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const cardShareButton = document.querySelector(".card__share");
 const cardAuthor = document.querySelector(".card__author");
 const cardAuthorInfo = document.querySelector(".card__author-info");
 const cardSocialShare = document.querySelector(".card__social-share");
+const cardSocialShareIcon = document.querySelectorAll(
+  ".card__social-share-icon"
+);
 
 function applyShareButtonStyles() {
   const cardShareButtonImg = cardShareButton.querySelector("img");
@@ -15,13 +18,32 @@ function resetShareButtonStyles() {
   cardShareButtonImg.style.cssText = "filter: brightness(100%)";
 }
 
+function enableTabIcon() {
+  cardSocialShareIcon.forEach((icon) => {
+    icon.setAttribute("tabindex", "0");
+  });
+}
+
+function disableTabIcon() {
+  cardSocialShareIcon.forEach((icon) => {
+    icon.setAttribute("tabindex", "-1");
+  });
+}
+
 function handleCardShareButton() {
   cardSocialShare.classList.toggle("card__social-share--active");
   if (cardSocialShare.classList.contains("card__social-share--active")) {
+    enableTabIcon();
     applyShareButtonStyles();
   } else {
     resetShareButtonStyles();
+    disableTabIcon();
   }
 }
 
 cardShareButton.addEventListener("click", handleCardShareButton);
+cardShareButton.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    handleCardShareButton();
+  }
+});
